36678c7d2a347eaba6484c687da643cbf81850df
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-12-04 21:01:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require 'pry'
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -10,3 +11,56 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp 
+  if input.to_i == 0
+    if songs.include?(input)
+      puts "Playing #{input}"
+    else
+      puts "Invalid input, please try again"
+    end
+  else
+    if input.to_i > 0 && input.to_i <= songs.length
+      puts "Playing #{songs[(input.to_i-1)]}"
+    else
+      puts "Invalid input, please try again"
+    end
+  end
+end 
+
+def list (songs)
+  songs.each_with_index do |track, i|
+    puts "#{i+1}. #{track}"
+  end 
+end 
+
+def exit_jukebox
+  puts "Goodbye"
+end 
+
+def run(songs)
+  help
+  loop do
+    puts "Please enter a command:"
+    command = gets.chomp
+    if command == "help"
+      help
+    elsif command == "list"
+      list(songs)
+    elsif command == "play"
+      play(songs)
+    elsif command == "exit"
+      exit_jukebox
+    end
+    break if command == "exit"
+  end
+end
